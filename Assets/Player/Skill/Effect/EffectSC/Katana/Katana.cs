a3744563bb06b8c701d4e133ad799e4686c3005b
KINOKO\nubob <Ki28ryu@icloud.com>
2025-12-02 23:22:49 +0900

    [add]刀をスポーンさせれるようになった

--- Assets/Player/Skill/Effect/EffectSC/Katana/Katana.cs
+++ Assets/Player/Skill/Effect/EffectSC/Katana/Katana.cs
@@ -2,10 +2,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "KatanaEffect", menuName = "Game/SkillEffect/GatKatana")]
 public class Katana : SkillEffect
 {
-    [SerializeField] private GameObject _katana;
-
     public override void Apply(PlayerStatus player)
     {
-        _katana.SetActive(true);
+        player.KatanaObject.SetActive(true);
     }
 }
