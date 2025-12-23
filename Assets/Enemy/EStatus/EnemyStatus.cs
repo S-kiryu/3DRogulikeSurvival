9ef4bfbc8407d67580b3009ab6b8fa6d0863c651
KINOKO\nubob <Ki28ryu@icloud.com>
2025-12-23 13:34:32 +0900

    [add]敵がダメージを食らうようにした

---
+++ Assets/Enemy/EStatus/EnemyStatus.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class EnemyStatus : MonoBehaviour
+{
+    [SerializeField] private StatusSettings _statusSettings;
+
+    public int CurrentHealth { get; private set; }
+    public int MaxHealth => _statusSettings.MaxHealth;
+    public float AttackPower => _statusSettings.AttackPower;
+
+    private void Awake()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    /// <summary>
+    /// ƒ_ƒ[ƒW‚ğó‚¯‚é
+    /// </summary>
+    /// <param name="damage"></param>
+    public void TakeDamage(int damage)
+    {
+        CurrentHealth -= damage;
+
+        if (CurrentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die() 
+    {
+        Destroy(gameObject);
+    }
+}
