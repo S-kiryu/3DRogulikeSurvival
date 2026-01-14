7139b76950068d9a074b01d01e990ea778422e8c
KINOKO\nubob <Ki28ryu@icloud.com>
2026-01-14 18:52:06 +0900

    [add]敵のスポーンを調整、Xpのすくりぷとを適用

--- Assets/Shop/Item/GetXp.cs
+++ Assets/Shop/Item/GetXp.cs
@@ -3,16 +3,21 @@ using UnityEngine;
 public class GetXp : MonoBehaviour
 {
     [SerializeField] private int _xpAmount = 10;
-    [SerializeField] private PlayerStatus _playerStatus;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-                _playerStatus.AddExp(_xpAmount);
+            PlayerStatus playerStatus = other.GetComponent<PlayerStatus>();
+
+            // プレイヤーステータスが存在し、生存している場合にXPを加算
+            if (playerStatus != null && playerStatus.IsAlive) 
+            {
+                playerStatus.AddExp(_xpAmount);
                 Debug.Log("プレイヤーがXPを取得しました！");
                 Destroy(gameObject);
-            
+
+            }
         }
     }
 }
