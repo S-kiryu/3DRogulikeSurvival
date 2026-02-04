fb3c52dd8adb4bb4179dc4ad5ff7ae8e9a9cf4dc
DESKTOP-1V2RDC8\PC_User <Ki28ryu@icloud.com>
2026-02-04 21:25:26 +0900

    no message

---
+++ Assets/Player/Skill/Effects/EffectSC/AttackBase.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public abstract class AttackBase : MonoBehaviour, IAttack
+{
+    protected PlayerStatus _playerStatus;
+
+    protected virtual void Awake()
+    {
+        _playerStatus = GetComponentInParent<PlayerStatus>();
+        
+        if (_playerStatus == null)
+        {
+            Debug.LogError("PlayerStatus ‚ªe‚ÉŒ©‚Â‚©‚è‚Ü‚¹‚ñ");
+        }
+
+    }
+
+    public abstract EffectType Type { get; }
+
+    public abstract float CoolTime { get; }
+
+    public abstract void Attack();
+}
