1e7082883a60975b3a32c61ef4bccc86fec7ad5f
KINOKO\nubob <Ki28ryu@icloud.com>
2026-02-17 01:55:05 +0900

    にほんご

--- Assets/Shop/Item/Kabotya.cs
+++ Assets/Shop/Item/Kabotya.cs
@@ -3,9 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "KabotyItem", menuName = "ScriptableObjects/Item/KabotyItem")]
 public class KabotyItem : ItemAction
 {
-
     public override void Execute(ItemData item)
     {
-        Debug.Log("‚©‚Ú‚¿‚á‚ğw“ü");
+        PlayerStatusManager.Instance.AddAttack(5);
     }
 }
